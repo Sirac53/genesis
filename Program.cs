193e8e20491d0d72421916512aaf3a9cdf50ee54
Sirac53 <145872412+Sirac53@users.noreply.github.com>
2023-10-22 19:07:40 +0300

    Add files via upload
    Skylab proje son web Apı

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+namespace Skylab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir  metin GİRİN: ");
+            string metin = Console.ReadLine();
+            Console.WriteLine("Virgülle ayırarak Bir  Tarih GİRİN: ");
+            string tarihBaslangic= Console.ReadLine();
+
+
+            {
+                var newsApiClient = new NewsApiClient("cc7539e648194a6d9029403c414713ab");
+                var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+                {
+                    Q = metin,
+                    SortBy = SortBys.Popularity,
+                    Language = Languages.EN,
+                    From = DateTime.Parse(tarihBaslangic)
+                }); 
+
+                if (articlesResponse.Status == Statuses.Ok)
+                {
+                    
+                    Console.WriteLine("Bulunan toplam makale: " + articlesResponse.TotalResults);
+                    
+                    foreach (var article in articlesResponse.Articles)
+                    {
+                        
+                        Console.WriteLine("Article: " + article.Title);
+                        
+                        Console.WriteLine("Yazarı: " + article.Author);
+                       
+                        Console.WriteLine("Acıklaması " + article.Description);
+                      
+                        Console.WriteLine("Urlsi: " + article.Url);
+                       
+                        Console.WriteLine("Yayınlanma zamanı: " + article.PublishedAt);
+
+                        Console.WriteLine("\n");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hata mesajı: " + articlesResponse.Error.Message);
+                }
+
+                Console.ReadLine();
+            }
+
+
+            }
+        }
+    }
